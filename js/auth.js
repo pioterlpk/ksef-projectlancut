9456408fdff1dabaa994f717b688e8f3b488cf73
pioterlpk <wilczeklpk@gmail.com>
2026-02-24 12:11:57 +0100

    Add files via upload

---
+++ js/auth.js
@@ -0,0 +1,11 @@
+function login() {
+    const user = document.getElementById("username").value;
+    const pass = document.getElementById("password").value;
+
+    // Przykładowe hasło statyczne
+    if(user === "admin" && pass === "1234") {
+        window.location.href = "dashboard.html";
+    } else {
+        document.getElementById("error-msg").innerText = "Nieprawidłowe dane!";
+    }
+}
